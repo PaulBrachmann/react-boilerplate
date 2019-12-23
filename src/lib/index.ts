91a73b53589a5fb1be1c3418185e20ced8209b71
Paul Brachmann <paul@enormos.com>
2019-12-23 20:01:32 +0100

    Fix lib exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,13 @@
 export * from "./color";
+
 export * from "./i18n";
+export { default as i18n } from "./i18n";
+
 export * from "./navigation";
+export { default as Navigation } from "./navigation";
+
 export * from "./platform";
+export { default as Platform } from "./platform";
+
 export * from "./style";
+export { default as StyleSheet } from "./style";
